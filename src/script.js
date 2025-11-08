135d343f9d0fea49adcc73f38d7af85002cde659
choi <icephoenixt2@gmail.com>
2025-11-08 15:46:41 -0300

    adicionando a lógica para buscar os dados na API

--- src/script.js
+++ src/script.js
@@ -9,15 +9,61 @@ function changeText(id, text) {
 
 // Daqui para baixo voce ira escrever
 // o código para resolver o desafio
+const url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1292";
+const detail_url = "https://pokeapi.co/api/v2/pokemon/";
+
+const id_img = "img_sprite_front_default";
+const id_name = "name";
+
+let listaPokemon = [];
+let pokemonAtual = 0;
+
+//função para pegar a lista de pokemons da API
+async function pegarListaPokemon() {
+  const response = await fetch(url);
+  const data = await response.json();
+  listaPokemon = data.results; //armazenar a lista de pokemons na variavel global
+}
+
+async function mostrarPokemon(pokemon) {
+  //mostra "carregando..." enquanto busca os dados para ficar mais interativo
+  changeText(id_name, "Carregando...");
+  changeImage(id_img, "");
+  //pega os detalhes do pokemon específico
+  const response = await fetch(detail_url + pokemon);
+  const data = await response.json();
+  //troca o nome e imagem do pokemon atual
+  changeText(id_name, data.name);
+  changeImage(id_img, data.sprites.front_default);
+}
 
 function previousPokemon() {
-  alert("Pokemon Anterior");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Anterior");
+  pokemonAtual--; //diminui o index do pokemon atual
+  //wrap-around
+  if (pokemonAtual < 0) {
+    pokemonAtual = listaPokemon.length - 1;
+  }
+  //troca o nome e imagem do pokemon atual
+  const nomePokemon = listaPokemon[pokemonAtual].name;
+  mostrarPokemon(nomePokemon);
 }
 
 function nextPokemon() {
-  alert("Pokemon Seguinte");
-  //abra o terminal em inspecionar no chrome para visualizar
-  console.log("Pokemon Seguinte");
+  pokemonAtual++; //aumenta o index do pokemon atual
+  //wrap-around
+  if (pokemonAtual >= listaPokemon.length) {
+    pokemonAtual = 0;
+  }
+  //troca o nome e imagem do pokemon atual
+  const nomePokemon = listaPokemon[pokemonAtual].name;
+  mostrarPokemon(nomePokemon);
 }
+
+//função principal para iniciar na ordem correta
+async function main() {
+  await pegarListaPokemon(); //pega a lista de pokemons
+  const primeiroPokemon = listaPokemon[pokemonAtual].name; //pega o nome do primeiro pokemon
+  await mostrarPokemon(primeiroPokemon); //mostra o primeiro pokemon
+}
+
+main(); //inicia o programa
